dc73feed4893fb20dd9e7f68964c67a05e77ea07
endepointe <endepointe@gmail.com>
2023-11-20 07:43:01 -0800

    see backlog for tls info

--- server.py
+++ server.py
@@ -37,32 +37,44 @@ Steps to consider:
     12. TLS session is established.
 
 '''
+import ssl
 import socket
 
-server_ip = '0.0.0.0'
-server_port = 45612
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    printf(f"Received: {requiest}")
+    if not request:
+        return
+    print(f"Received: {request.decode()}")
+    client_socket.send(b"Server hello")
+    client_socket.close()
+ 
+
+
+server_address = ('0.0.0.0', 45612)
+certfile = ""
+keyfile = ""
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_socket.bind((server_ip,server_port))
+server_socket.bind(server_address)
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile=certfile,keyfile=keyfile)
+context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 #disable tls v1 and v1.1
 
 server_socket.listen(5)
 
 print(f"Listening on {server_ip}:{server_port}...")
 
-while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"Accecpted connection from {client_address[0]}:{client_address[1]}")
+try:
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Accecpted connection from {client_address[0]}:{client_address[1]}")
 
-    data = client_socket.recv(1024)
-    
-    if not data:
-        break
+        ssl_client_socket = context.wrap_socket(client_socket, server_side=True)
 
-    print(f"Received: {data.decode()}")
+        handle_client(ssl_client_socket)
 
-    client_socket.send(data)
-
-    client_socket.close()
-
-server_socket.close()
+finally:
+    server_socket.close()
